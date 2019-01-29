e79d2f18b7443c5ce88dfd665c0395ee8bd4f767
Duncan <buzzbuzz.macdonald@gmail.com>
2019-01-29 15:13:56 -0800

    finished grains challenge

--- grains/grains.go
+++ grains/grains.go
@@ -4,19 +4,25 @@ import (
 	"errors"
 )
 
-func Square(in uint64) (uint64, error) {
-	if in > 64 || in < 0 {
+func Square(in int) (uint64, error) {
+	if in > 64 || in <= 0 {
 		return 0, errors.New("Input out of range")
 	}
 
-	return in * in, nil
+	var lastnum uint64 = 1
+
+	for i := 2; i <= in; i++ {
+		lastnum = lastnum * 2
+	}
+
+	return lastnum, nil
 }
 
 func Total() uint64 {
 	var sum uint64
 
 	for i := 0; i <= 64; i++ {
-		square, _ := Square(uint64(i))
+		square, _ := Square(i)
 		sum += square
 	}
 
